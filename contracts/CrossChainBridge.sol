6c35e05650ba31e9947f806fdacc6fed0981182b
Tony Stonks <stonkstony1@gmail.com>
2026-02-12 16:36:22 +0200

    Update CrossChainBridge.sol

--- contracts/CrossChainBridge.sol
+++ contracts/CrossChainBridge.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
