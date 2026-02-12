f08d827b9013f93fdf60b3ce6358b2566f021f07
Tony Stonks <stonkstony1@gmail.com>
2026-02-12 16:32:37 +0200

    Update CrossChainBridge.sol

--- contracts/CrossChainBridge.sol
+++ contracts/CrossChainBridge.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/security/Pausable.sol";
 
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract CrossChainBridge is Ownable, Pausable, ReentrancyGuard {
