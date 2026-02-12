b28e7120225992a5212197cca3950bdb1979a0f7
Tony Stonks <stonkstony1@gmail.com>
2026-02-12 16:32:24 +0200

    Update CrossChainBridge.sol

--- contracts/CrossChainBridge.sol
+++ contracts/CrossChainBridge.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
+
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
